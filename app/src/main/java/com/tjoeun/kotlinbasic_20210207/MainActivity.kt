48d2dba33cfbab3966508ed13f54d8368d48f62a
seoyeongkang <jumprope123@naver.com>
2021-02-07 16:38:08 +0900

    전송 버튼 눌리면 => 로그 남기기

--- app/src/main/java/com/tjoeun/kotlinbasic_20210207/MainActivity.kt
+++ app/src/main/java/com/tjoeun/kotlinbasic_20210207/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.kotlinbasic_20210207
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sendBtn
+        sendBtn.setOnClickListener {
+
+//            Ctrl + ㅣ로 이줄을 주석으로.
+//            sendBtn이 눌리면 싱행해준 코드
+
+            Log.d("메인 화면" , "전송버튼이 눌림")
+
+        }
+
     }
 }
